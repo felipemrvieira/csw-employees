6a4a02f9c532e43e3eea6f80a39a0a6fec0a1456
felipemrvieira <felipemrvieira@gmail.com>
2022-05-11 13:10:05 +0100

    Create form items

--- src/components/ActionButtons/index.js
+++ src/components/ActionButtons/index.js
@@ -1,15 +1,192 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+// @ts-nocheck
+import React, { useState } from 'react'
+import { Button, Modal, Icon } from 'semantic-ui-react'
+import { useForm } from 'react-hook-form'
+import DatePicker from 'react-datepicker'
 
-import { Container } from './styles'
+import 'react-datepicker/dist/react-datepicker.css'
+
+import {
+  Container,
+  Label,
+  LabelWrapper,
+  InputWrapper,
+  Field,
+  RadioItemsWrapper,
+  RadioWrapper,
+  RadioLabel,
+} from './styles'
+
+function exampleReducer(state, action) {
+  switch (action.type) {
+    case 'OPEN_MODAL':
+      return { open: true, dimmer: action.dimmer }
+    case 'CLOSE_MODAL':
+      return { open: false }
+    default:
+      throw new Error()
+  }
+}
 
 function ActionButtons() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
+  const [state, dispatch] = React.useReducer(exampleReducer, {
+    open: false,
+    dimmer: undefined,
+  })
+  const { open, dimmer } = state
+
+  const [startDate, setStartDate] = useState(new Date())
+
   return (
     <Container>
       <div>
-        <Button content='Add' icon='add' labelPosition='left' />
+        <Button
+          content='Add'
+          icon='add'
+          labelPosition='left'
+          onClick={() => dispatch({ type: 'OPEN_MODAL' })}
+        />
         <Button content='Edit' icon='edit' labelPosition='left' />
         <Button content='Delete' icon='delete' labelPosition='left' />
+
+        <Modal
+          dimmer={dimmer}
+          open={open}
+          size='tiny'
+          onClose={() => dispatch({ type: 'CLOSE_MODAL' })}
+        >
+          <Modal.Header>Add Employee</Modal.Header>
+          <Modal.Content>
+            <div>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='form_content'>
+                  <Field>
+                    <LabelWrapper>
+                      <Label>Name</Label>
+                    </LabelWrapper>
+                    <InputWrapper>
+                      {errors.firstName?.type === 'required' &&
+                        'First name is required'}
+
+                      <input
+                        {...register('firstName', {
+                          required: true,
+                          maxLength: 20,
+                        })}
+                      />
+                    </InputWrapper>
+                  </Field>
+
+                  <Field>
+                    <LabelWrapper>
+                      <Label>Start Date</Label>
+                    </LabelWrapper>
+                    <InputWrapper>
+                      {errors.firstName?.type === 'required' &&
+                        'First name is required'}
+                      <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                      />
+                      <Icon name='calendar alternate outline' />
+                    </InputWrapper>
+                  </Field>
+
+                  <Field>
+                    <LabelWrapper>
+                      <Label>Role</Label>
+                    </LabelWrapper>
+                    <InputWrapper>
+                      {errors.firstName?.type === 'required' &&
+                        'First name is required'}
+                      <RadioItemsWrapper>
+                        <RadioWrapper>
+                          <input
+                            {...register('teste')}
+                            type='radio'
+                            value='(none selected)'
+                          />
+                          <RadioLabel>(none selected)</RadioLabel>
+                        </RadioWrapper>
+                        <RadioWrapper>
+                          <input
+                            {...register('teste')}
+                            type='radio'
+                            value='asd'
+                          />
+                          <RadioLabel>JE</RadioLabel>
+                        </RadioWrapper>
+                        <RadioWrapper>
+                          <input
+                            {...register('teste')}
+                            type='radio'
+                            value='asd'
+                          />
+                          <RadioLabel>PE</RadioLabel>
+                        </RadioWrapper>
+                        <RadioWrapper>
+                          <input
+                            {...register('teste')}
+                            type='radio'
+                            value='asd'
+                          />
+                          <RadioLabel>SE</RadioLabel>
+                        </RadioWrapper>
+                        <RadioWrapper>
+                          <input
+                            {...register('teste')}
+                            type='radio'
+                            value='asd'
+                          />
+                          <RadioLabel>TM</RadioLabel>
+                        </RadioWrapper>
+                      </RadioItemsWrapper>
+                    </InputWrapper>
+                  </Field>
+
+                  <Field>
+                    <LabelWrapper>
+                      <Label>Platoon</Label>
+                    </LabelWrapper>
+                    <InputWrapper>
+                      {errors.firstName?.type === 'required' &&
+                        'First name is required'}
+                      <select name='func'>
+                        <option value='5'>Function 2</option>
+                        <option value='6'>Function 3</option>
+                      </select>
+                    </InputWrapper>
+                  </Field>
+
+                  {/* <input {...register('lastName', { pattern: /^[A-Za-z]+$/i })} />
+                <input
+                  type='number'
+                  {...register('age', { min: 18, max: 99 })}
+                />
+                <input type='submit' /> */}
+                </div>
+              </form>
+            </div>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button primary onClick={() => dispatch({ type: 'CLOSE_MODAL' })}>
+              Add
+            </Button>
+            <Button onClick={() => dispatch({ type: 'CLOSE_MODAL' })}>
+              Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </div>
     </Container>
   )
--- src/components/ActionButtons/styles.js
+++ src/components/ActionButtons/styles.js
@@ -4,4 +4,45 @@ export const Container = styled.div`
   display: flex;
   width: 100%;
   padding: 10px 18px 0px 18px;
+  .form_content {
+    display: flex;
+  }
+`
+export const Field = styled.div`
+  width: 100%;
+  display: flex;
+  margin-bottom: 8px;
+`
+export const LabelWrapper = styled.div`
+  width: 33%;
+  display: flex;
+  align-items: flex-start;
+  margin-right: 12px;
+`
+export const Label = styled.label`
+  width: 100%;
+  text-align: right;
+`
+
+export const InputWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  /* input {
+    width: 100%;
+  } */
+`
+
+export const RadioItemsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const RadioWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+export const RadioLabel = styled.label`
+  margin-left: 3px;
 `
